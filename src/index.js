4ab48f4623b0b0d6d0ac64823873e870890b4dd6
Roland <rolandgolla@gmail.com>
2023-09-11 23:41:46 +0200

    Refactoring

--- src/index.js
+++ src/index.js
@@ -4,19 +4,3 @@ import "reveal.js/dist/theme/black.css";
 import "./css/nca.css";
 
 Reveal.initialize();
-
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("/service-worker.js").then(
-      (registration) => {
-        console.log(
-          "ServiceWorker registration successful with scope: ",
-          registration.scope
-        );
-      },
-      (err) => {
-        console.log("ServiceWorker registration failed: ", err);
-      }
-    );
-  });
-}
--- webpack.config.js
+++ webpack.config.js
@@ -1,87 +1,58 @@
 const path = require("path");
+const fs = require("fs");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
-const fs = require("fs");
 
-const headerTemplate = fs.readFileSync(
-  path.resolve(__dirname, "./src/includes/header.ejs"),
-  "utf8"
-);
-const footerTemplate = fs.readFileSync(
-  path.resolve(__dirname, "./src/includes/footer.ejs"),
-  "utf8"
-);
-
-const generateHtmlPlugins = (templateDir) => {
-  const templateFiles = fs.readdirSync(path.resolve(__dirname, templateDir));
-  return templateFiles
-    .filter((item) => item.endsWith(".html") || item.endsWith(".ejs"))
-    .map((item) => {
-      const parts = item.split(".");
-      const name = parts[0];
-      const extension = parts[1];
-      return new HtmlWebpackPlugin({
-        filename: `${name}.html`,
-        template: path.resolve(
-          __dirname,
-          `${templateDir}/${name}.${extension}`
-        ),
-        templateParameters: {
-          header: headerTemplate,
-          footer: footerTemplate,
-        },
-      });
+const templates = fs
+  .readdirSync(path.resolve(__dirname, "src/presentations"))
+  .map((file) => {
+    return new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, `src/presentations/${file}`),
+      filename: file.replace(".ejs", ".html"),
     });
-};
-
-const htmlPlugins = generateHtmlPlugins("./src/presentations");
+  });
 
 module.exports = {
   mode: "development",
   entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, "docs"),
-    filename: "bundle.js",
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
   },
   module: {
     rules: [
       {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-      {
         test: /\.ejs$/,
         use: [
           {
-            loader: "ejs-loader",
-            options: {
-              esModule: false,
-              variable: "data",
-            },
+            loader: "ejs-compiled-loader",
           },
         ],
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
       },
     ],
   },
   plugins: [
-    ...htmlPlugins,
-    new CopyWebpackPlugin({
-      patterns: [
-        {
-          from: "src/content",
-          to: "content",
-        },
-      ],
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "src/start.ejs"),
+      filename: "index.html",
     }),
+    ...templates,
     new CopyWebpackPlugin({
-      patterns: [{ from: "src/service-worker.js", to: "service-worker.js" }],
+      patterns: [{ from: "src/content", to: "content" }],
     }),
   ],
   devServer: {
-    static: path.join(__dirname, "docs"),
-    compress: true,
-    port: 8000,
-    hot: true,
+    static: {
+      directory: path.resolve(__dirname, "src"),
+    },
   },
-  devtool: "source-map",
+  optimization: {
+    splitChunks: {
+      chunks: "all",
+    },
+  },
 };
