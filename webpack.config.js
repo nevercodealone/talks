84ddd6da36192b9673db76812526f373fa762f7f
Roland <rolandgolla@gmail.com>
2023-09-17 19:33:29 +0200

    Use docs folder in webpack config, regenarate files and remove dist.

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
   entry: "./src/index.js",
   output: {
     filename: "[name].[contenthash].js",
-    path: path.resolve(__dirname, "dist"),
+    path: path.resolve(__dirname, "docs"),
   },
   module: {
     rules: [
