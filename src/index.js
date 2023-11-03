7660f8af73c59c720caae7fdc931cf8796184d35
Roland <rolandgolla@gmail.com>
2023-11-03 16:09:34 +0100

    Use new reveal version and refactor build process.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import Reveal from "reveal.js";
+import RevealHighlight from "reveal.js/plugin/highlight/highlight";
 import "reveal.js/dist/reveal.css";
 import "reveal.js/dist/theme/black.css";
+import "highlight.js/styles/monokai-sublime.css";
 import "./css/nca.css";
 
-Reveal.initialize();
+Reveal.initialize({
+  plugins: [RevealHighlight],
+});
--- webpack.config.js
+++ webpack.config.js
@@ -2,57 +2,67 @@ const path = require("path");
 const fs = require("fs");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 const templates = fs
   .readdirSync(path.resolve(__dirname, "src/presentations"))
-  .map((file) => {
-    return new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, `src/presentations/${file}`),
-      filename: file.replace(".ejs", ".html"),
-    });
-  });
+  .filter(file => file.endsWith('.ejs'))
+  .map(file => new HtmlWebpackPlugin({
+    template: path.resolve(__dirname, `src/presentations/${file}`),
+    filename: file.replace(".ejs", ".html"),
+  }));
 
-module.exports = {
-  mode: "development",
-  entry: "./src/index.js",
-  output: {
-    filename: "[name].[contenthash].js",
-    path: path.resolve(__dirname, "docs"),
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ejs$/,
-        use: [
-          {
-            loader: "ejs-compiled-loader",
-          },
-        ],
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
+module.exports = (env, argv) => {
+  const isDev = argv.mode === 'development';
+
+  return {
+    mode: isDev ? "development" : "production",
+    entry: "./src/index.js",
+    output: {
+      filename: isDev ? "[name].js" : "[name].[contenthash].js",
+      path: path.resolve(__dirname, "docs"),
+      clean: true, // Clean the output directory before emit.
+    },
+    devtool: isDev ? 'eval-source-map' : 'source-map',
+    module: {
+      rules: [
+        {
+          test: /\.ejs$/,
+          use: ["ejs-compiled-loader"],
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+      ],
+    },
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: path.resolve(__dirname, "src/start.ejs"),
+        filename: "index.html",
+      }),
+      ...templates,
+      new CopyWebpackPlugin({
+        patterns: [{ from: "src/content", to: "content" }],
+      }),
     ],
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, "src/start.ejs"),
-      filename: "index.html",
-    }),
-    ...templates,
-    new CopyWebpackPlugin({
-      patterns: [{ from: "src/content", to: "content" }],
-    }),
-  ],
-  devServer: {
-    static: {
-      directory: path.resolve(__dirname, "src"),
+    devServer: {
+      static: {
+        directory: path.resolve(__dirname, "src"),
+      },
+      hot: true,
     },
-  },
-  optimization: {
-    splitChunks: {
-      chunks: "all",
+    optimization: {
+      splitChunks: {
+        chunks: "all",
+      },
     },
-  },
+    performance: {
+        hints: false,
+        maxEntrypointSize: Infinity,
+        maxAssetSize: Infinity,
+    },  
+  };
 };
+
